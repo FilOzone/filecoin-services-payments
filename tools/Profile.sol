a4fba2dd60b10c35cd5e32773e4dbaae3d3eb23b
William Morriss <wjmelements@gmail.com>
2025-08-05 15:52:27 -0500

    create gas profile

--- tools/Profile.sol
+++ tools/Profile.sol
@@ -6,21 +6,42 @@ import "../src/Payments.sol";
 import "../test/mocks/MockERC20.sol";
 
 contract Profile is Script {
-    function run() public {
+    function run(address sender) public {
         vm.startBroadcast();
 
         ERC20 token = new MockERC20("MockToken", "MOCK");
 
         Payments payments = new Payments();
 
-        address from = address(this);
-        address to = address(this);
-        address validator = address(this);
+        address from = sender;
+        address to = sender;
+        address operator = sender;
+        address validator = sender;
 
         uint256 commissionRateBps = 0;
-        address serviceFeeRecipient = address(this);
+        address serviceFeeRecipient = sender;
         
-        //payments.createRail(address(token), from, to, validator, commissionRateBps, serviceFeeRecipient);
+        uint256 rateAllowance = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+        uint256 lockupAllowance = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+        uint256 maxLockupPeriod = 0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+
+        payments.setOperatorApproval(address(token), operator, true, rateAllowance, lockupAllowance, maxLockupPeriod);
+
+        payments.createRail(address(token), from, to, validator, commissionRateBps, serviceFeeRecipient);
+
+
+        // deposit
+        // depositWithPermit
+        // depositWithPermitAndApproveOperator
+        // depositWithPermitAndIncreaseOperatorApproval
+        // modifyRailPayment
+        // modifyRailLockup
+        // increaseOperatorApproval
+        // settleRail
+        // withdraw
+        // withdrawTo
+        // terminateRail
+        // settleTerminatedRailWithoutValidation
 
         vm.stopBroadcast();
     }
--- tools/profile.sh
+++ tools/profile.sh
@@ -4,5 +4,8 @@ pushd ../localnet
 source ./local_lotus.sh
 popd
 
-forge script -vvv -g 20000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 tools/Profile.sol:Profile
+forge script -vvv -g 20000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "run(address)" tools/Profile.sol:Profile $SENDER_ADDRESS
 
+
+
+../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.sol/$CHAIN_ID/run-latest.json | tee .gas-profile
