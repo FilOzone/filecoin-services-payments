e8b4086b5623acc98e6c73490555f985844d7122
William Morriss <wjmelements@gmail.com>
2025-08-08 13:32:31 -0500

    rename script to .s.sol

--- tools/profile.sh
+++ tools/profile.sh
@@ -10,19 +10,19 @@ popd
 TOKEN_ADDRESS=0xe9ae74e0c182aab11bddb483227cc1f6600b3625
 RAIL_ADDRESS=0xf6990c51dc94b36c5d5184bf60107efe99dde592
 
-forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "createRail(address)" tools/Profile.sol:Profile $SENDER_ADDRESS
+forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "createRail(address)" tools/Profile.s.sol:Profile $SENDER_ADDRESS
 
-../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.sol/$CHAIN_ID/createRail-latest.json | tee .gas-profile
-
-sleep 35
-
-
-forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "settleRail(address,address,uint256)" tools/Profile.sol:Profile $SENDER_ADDRESS $RAIL_ADDRESS 1
-
-../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.sol/$CHAIN_ID/settleRail-latest.json | tee -a .gas-profile
+../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.s.sol/$CHAIN_ID/createRail-latest.json | tee .gas-profile
 
 sleep 35
 
-forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "terminateRail(address,address,uint256)" tools/Profile.sol:Profile $SENDER_ADDRESS $RAIL_ADDRESS 1
 
-../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.sol/$CHAIN_ID/terminateRail-latest.json | tee -a .gas-profile
+forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "settleRail(address,address,uint256)" tools/Profile.s.sol:Profile $SENDER_ADDRESS $RAIL_ADDRESS 1
+
+../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.s.sol/$CHAIN_ID/settleRail-latest.json | tee -a .gas-profile
+
+sleep 35
+
+forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "terminateRail(address,address,uint256)" tools/Profile.s.sol:Profile $SENDER_ADDRESS $RAIL_ADDRESS 1
+
+../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.s.sol/$CHAIN_ID/terminateRail-latest.json | tee -a .gas-profile
