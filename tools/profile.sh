1808eb7368903b65331c8e5e2a601747750ad9a0
William Morriss <wjmelements@gmail.com>
2025-08-08 14:30:36 -0500

    can sleep 5 instead of 10

--- tools/profile.sh
+++ tools/profile.sh
@@ -14,14 +14,14 @@ forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADD
 
 ../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.s.sol/$CHAIN_ID/createRail-latest.json | tee .gas-profile
 
-sleep 10
+sleep 5
 
 
 forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "settleRail(address,address,uint256)" tools/Profile.s.sol:Profile $SENDER_ADDRESS $RAIL_ADDRESS 1
 
 ../forge-script-gas-report/forge_script_gas_report ./broadcast/Profile.s.sol/$CHAIN_ID/settleRail-latest.json | tee -a .gas-profile
 
-sleep 10
+sleep 5
 
 forge script -vvv -g 44000 --broadcast --chain-id $CHAIN_ID --sender $SENDER_ADDRESS --private-key $SENDER_KEY --rpc-url $API_URL/rpc/v1 --sig "terminateRail(address,address,uint256)" tools/Profile.s.sol:Profile $SENDER_ADDRESS $RAIL_ADDRESS 1
 
